e69690155534ddab53706a280d4a7607b25f92de
mtanlee <mtanlee07@gmail.com>
2016-04-15 22:50:36 +0800

    删除dockerclient包

--- event.go
+++ event.go
@@ -3,12 +3,10 @@ package distributionweb
 import (
 	"time"
 
-	"github.com/mtanlee/distributionweb/Godeps/_workspace/src/github.com/samalba/dockerclient"
 )
 
 type Event struct {
 	Type          string                      `json:"type,omitempty"`
-	ContainerInfo *dockerclient.ContainerInfo `json:"container_info,omitempty"`
 	Time          time.Time                   `json:"time,omitempty"`
 	Message       string                      `json:"message,omitempty"`
 	Username      string                      `json:"username,omitempty"`
